73d37ee05c3c180f73d906f3df621dd6a58a1b97
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-08-08 10:16:02 +0530

    Add 1.Basic folder with initial C programs

---
+++ 1.Basic/Variables_and_Datatypes.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int x = 5;
+    printf("%d \n",x);
+
+    float y = 3.14;
+    printf("%f \n",y);
+
+    double z = 7.4;
+    printf("%d \n", z);
+
+    char ch = 'A';
+    printf("%c \n", ch);
+
+    bool b = true;
+    printf("%d \n", b);
+    
+    return 0;
+
+}
