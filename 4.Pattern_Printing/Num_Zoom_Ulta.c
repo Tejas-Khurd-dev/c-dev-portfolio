dd7db64d1747ee1278ca0a57828b2e1aeede8bc3
Tejas Khurd <tejaskhurd.dev@gmail.com>
2025-08-19 11:44:42 +0530

    Add pattern printing programs
    Num_Zoom, Num_Zoom_Ulta

---
+++ 4.Pattern_Printing/Num_Zoom_Ulta.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number of row: ");
+    scanf("%d",&n);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            int min = i;
+            if(j < min) min = j;                 
+            if(n - j + 1 < min) min = n - j + 1;  
+            if(n - i + 1 < min) min = n - i + 1; 
+            printf("%d " , n-min+1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
